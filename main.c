2ddac789526d6ae912f28a06f1debbbd19cef9b8
cengiz <onkal.cengiz@gmail.com>
2019-11-07 21:21:12 +0200

    first

--- main.c
+++ main.c
@@ -38,56 +38,32 @@ void func(int sockfd) {
     }
 }
 
+
 // Driver function
 int main() {
     int sockfd, connfd, len;
     struct sockaddr_in servaddr, cli;
 
+    //create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
-    } else {
-        printf("Socket successfully created..\n");
-    }
-
     //clear
     bzero(&servaddr, sizeof(servaddr));
 
-    // assign IP, PORT
+    //assign address
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
+    //bind socket
+    bind(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
-    // Binding newly created socket to given IP and verification
-    if ((bind(sockfd, (SA *) &servaddr, sizeof(servaddr))) != 0) {
-        printf("socket bind failed...\n");
-        exit(0);
-    } else {
-        printf("Socket successfully binded..\n");
-    }
-
-
-    // Now server is ready to listen and verification
-    if ((listen(sockfd, 5)) != 0) {
-        printf("Listen failed...\n");
-        exit(0);
-    } else{
-        printf("Server listening..\n");
-    }
-
+    //listen port
+    listen(sockfd, 5);
     len = sizeof(cli);
-    // Accept the data packet from client and verification
+    //accept connection
     connfd = accept(sockfd, (SA *) &cli, &len);
-    if (connfd < 0) {
-        printf("server acccept failed...\n");
-        exit(0);
-    } else
-        printf("server acccept the client...\n");
-
-    // Function for chatting between client and server
+    //run command
     func(connfd);
 
-    // After chatting close the socket
+    //clone socket
     close(sockfd);
 }
